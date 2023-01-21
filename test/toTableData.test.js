05c4c94191d4d9342333f4a71d12c4343dd72c38
BugbearR <bugbearr@bugbearr.jp>
2023-01-21 04:58:13 +0000

    Impove test

--- test/toTableData.test.js
+++ test/toTableData.test.js
@@ -5,5 +5,39 @@ test('Hello, world!', () => {
         { ka: "va1", kb: "vb1" },
         { ka: "va2", kb: "vb2" },
     ];
-    expect(toTableData(data)).toMatchSnapshot();
+    expect(toTableData(data)).toStrictEqual({
+        fields: [ "ka", "kb" ],
+        rows: [
+            [ "va1", "vb1" ],
+            [ "va2", "vb2" ]
+        ]
+    });
+});
+
+test('omit', () => {
+    const data = [
+        { ka: "va1", kb: "vb1", kc: "vc1", kd: "vd1" },
+        { ka: "va2", kb: "vb2", kc: "vc2", kd: "vd2" },
+    ];
+    expect(toTableData(data, { omit: [ "kb", "kd" ]})).toStrictEqual({
+        fields: [ "ka", "kc" ],
+        rows: [
+            [ "va1", "vc1" ],
+            [ "va2", "vc2" ]
+        ]
+    });
+});
+
+test('pick', () => {
+    const data = [
+        { ka: "va1", kb: "vb1", kc: "vc1", kd: "vd1" },
+        { ka: "va2", kb: "vb2", kc: "vc2", kd: "vd2" },
+    ];
+    expect(toTableData(data, { pick: [ "kb", "kd" ]})).toStrictEqual({
+        fields: [ "kb", "kd" ],
+        rows: [
+            [ "vb1", "vd1" ],
+            [ "vb2", "vd2" ]
+        ]
+    });
 });
